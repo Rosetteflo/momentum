9f40f301db942f3f152de3731f4c423caa80af67
Jeehyun park <87361133+Rosetteflo@users.noreply.github.com>
2021-09-05 12:41:07 +0900

    css
    momentum css

--- js/background.js
+++ js/background.js
@@ -1,11 +1,37 @@
-const images = ["pic1.webp","pic2.webp","pic3.webp"];
+const body = document.querySelector("body");
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
 
-const chosenImage = images [Math.floor(Math.random() * images.length)];
+window.onload = function () {
+    const firstColor = colors[Math.floor(Math.random() * colors.length)];
+    const secondColor = colors[Math.floor(Math.random() * colors.length)];
+    const mixColor = `linear-gradient(to right, ${firstColor}, ${secondColor})`;
+  
+    if (firstColor === secondColor) {
+      colorShow();
+    }
+  
+    body.style.background = mixColor;
+}
 
-const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`
 
-console.log(bgImage);
 
-document.body.appendChild(bgImage);
